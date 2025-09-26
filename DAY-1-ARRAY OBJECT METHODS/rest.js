a7de27fde3b39fcc7ced4fcfb1f74e055f41b2f0
JAY_PARMAR <jayparmarit28@gmail.com>
2025-09-26 12:06:23 +0530

    rest method

---
+++ DAY-1-ARRAY OBJECT METHODS/rest.js
@@ -0,0 +1,17 @@
+// rest
+
+// function total(a,b,c,d,e,f,g,h,i,j){
+
+//     const result = a+b+c+d+e+f+g+h+i+j;
+
+//     console.log(result)
+// }
+
+// total(1,2,3,4,5,6,7,8,9,10)
+
+
+function total(...arg){
+    console.log(...arg)
+}
+
+total(1,2,3,4,5,6,7,8,9,10)
