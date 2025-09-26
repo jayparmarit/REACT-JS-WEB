7865b549399a05ff75861f9d435e428d90bd1d56
JAY_PARMAR <jayparmarit28@gmail.com>
2025-09-26 11:56:33 +0530

    Add files via upload

---
+++ REACT-JS-WEB/DAY-1-ARRAY OBJECT METHODS/spreading.js
@@ -0,0 +1,41 @@
+// spread
+
+// const car = ["thar","scorpio","fortuner","verna"];
+
+// const name = ["krushnal","meet","amit","kartik"];
+
+// const total = [...car, ...name];
+
+// console.log(total)
+
+
+
+// object 
+
+// const person1 = {
+//     name:"rashmika",
+//     age:"25",
+//     city:"mumbai",
+// };
+// const person2 = {
+//     name1:"shradha",
+//     age1:"27",
+//     city1:"mumbai",
+// };
+
+// const peopleData = {...person1, ...person2};
+
+// console.log(peopleData);
+
+
+const item1 = {
+    number1:"one",
+    number2:"two"
+};
+
+const item2 = {
+    ...item1,
+    number3:"three"
+};
+
+console.log(item2)
