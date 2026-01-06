97bd350e8c1f8f208da07969bec757a0a793ad7e
JAY_PARMAR <jayparmarit28@gmail.com>
2026-01-06 10:53:54 +0530

    Add files via upload

---
+++ REDUX-THUNK/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserLists from "./Components/UserLists";
+
+const App = () => {
+  return (
+    <>
+      <UserLists />
+    </>
+  );
+};
+
+export default App;
