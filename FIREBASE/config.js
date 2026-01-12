4860e3954617e80ede45d552113b26ed44d1cbc1
JAY_PARMAR <jayparmarit28@gmail.com>
2026-01-12 13:32:43 +0530

    Add files via upload

--- FIREBASE/config.js
+++ FIREBASE/config.js
@@ -1,9 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getAuth,GoogleAuthProvider } from "firebase/auth";
+
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
@@ -13,5 +13,10 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APPID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const googleProvider = new GoogleAuthProvider();
+
+export const db = getFirestore(app)
