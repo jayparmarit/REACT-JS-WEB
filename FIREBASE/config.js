3ff1a8925e818d04eebf6e38aa573d9a0821784f
JAY_PARMAR <jayparmarit28@gmail.com>
2026-02-04 13:54:59 +0530

    Add files via upload

--- FIREBASE/config.js
+++ FIREBASE/config.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth,GoogleAuthProvider } from "firebase/auth";
 
-import { getFirestore } from "firebase/firestore";
+// import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
@@ -19,4 +19,4 @@ export const auth = getAuth(app);
 
 export const googleProvider = new GoogleAuthProvider();
 
-export const db = getFirestore(app)
+// export const db = getFirestore(app)
